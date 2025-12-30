a57442e78db3ee61197bb7ea00b28abd0108d0d2
JohnnyCPP <johnny.cpp@proton.me>
2025-12-30 23:25:43 +0100

    Added solution to cpp00.

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,8 +1,48 @@
 #include <cstdlib>
+#include <iostream>
+#include <cctype>
+
+const	std::string	NOISE = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+
+void	println(const std::string& str)
+{
+	std::cout << str << std::endl;
+}
+
+void	toUpper(std::string& str)
+{
+	int	length;
+	int	i;
+
+	length = static_cast<int>(str.length());
+	i = 0;
+	while (i < length)
+	{
+		str[i] = std::toupper(str[i]);
+		i ++;
+	}
+}
+
+void	printUpperCase(char **strList)
+{
+	std::string	message;
+	int			i;
+
+	if (!strList || !strList[0])
+		return ;
+	message = "";
+	i = 0;
+	while (strList[i])
+		message += strList[i ++];
+	toUpper(message);
+	println(message);
+}
 
 int	main(int argc, char **argv)
 {
-	(void) argc;
-	(void) argv;
+	if (argc == 1)
+		println(NOISE);
+	else
+		printUpperCase(argv + 1);
 	return (EXIT_SUCCESS);
 }
