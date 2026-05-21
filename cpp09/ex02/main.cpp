fac33269997341a0ceb3ac54afa526114d72a642
JohnnyCPP <johnny.cpp@proton.me>
2026-05-22 00:35:48 +0200

    Updated main of ex02 of cpp09.

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -7,16 +7,7 @@ int	main(int argc, char **argv)
 		std::cout << "Usage: ./PmergeMe <numbers>" << std::endl;
 		return (EXIT_FAILURE);
 	}
-	try
-	{
-		PmergeMe	pmergeMe(argv);
-
-		pmergeMe.sort();
-	}
-	catch (const std::exception& e)
-	{
-		std::cerr << "Error: " << e.what() << std::endl;
-		return (EXIT_FAILURE);
-	}
+	PmergeMe pmergeMe(argv);
+	pmergeMe.sort();
 	return (EXIT_SUCCESS);
 }
